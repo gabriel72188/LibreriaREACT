09481e50328d5acb9fd5b0adbf476ec3d70c5f64
Ismael <08062006ismael@gmail.com>
2025-04-17 17:48:27 +0200

    fix enrutamiento y cambio de estetica

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,60 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <header>
-    <h1 className="site-heading text-center text-faded d-none d-lg-block">
+      <h1 className="site-heading text-center text-faded d-none d-lg-block">
         <span className="site-heading-upper text-primary mb-3">Librería</span>
         <span className="site-heading-lower">Más cuento que Calleja</span>
-    </h1>
-    
-    <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      </h1>
+
+      <nav className="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
         <div className="container">
-            <a className="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">Librería</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mx-auto">
-                    <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" href="/">Inicio</a></li>
-                    <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" href="/about">Sobre</a></li>
-                    <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" href="/products">Libros</a></li>
-                    <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" href="/store">Horario</a></li>
-                    <li className="nav-item px-lg-4"><a className="nav-link text-uppercase" href="/login">Admin</a></li>
-                </ul>
-            </div>
+          <Link className="navbar-brand text-uppercase fw-bold d-lg-none" to="/">
+            Librería
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item px-lg-4">
+                <Link className="nav-link text-uppercase" to="/">
+                  Inicio
+                </Link>
+              </li>
+              <li className="nav-item px-lg-4">
+                <Link className="nav-link text-uppercase" to="/about">
+                  Sobre
+                </Link>
+              </li>
+              <li className="nav-item px-lg-4">
+                <Link className="nav-link text-uppercase" to="/products">
+                  Libros
+                </Link>
+              </li>
+              <li className="nav-item px-lg-4">
+                <Link className="nav-link text-uppercase" to="/store">
+                  Horario
+                </Link>
+              </li>
+              <li className="nav-item px-lg-4">
+                <Link className="nav-link text-uppercase" to="/login">
+                  Admin
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
-</header>
-  )
-}
+      </nav>
+    </header>
+  );
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,21 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
 import { Index } from '../pages/Index';
-import { About } from '../pages/about';
+import { About } from '../pages/About';
+import { Products } from '../pages/Products';
 import { Store } from '../pages/store';
 import { Login } from '../pages/Login';
-import { Products } from '../pages/Products';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
-    <BrowserRouter>
     <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/index" element={<Index />} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/products" element={<Products />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
-</BrowserRouter>  )
-}
-
+  );
+};
