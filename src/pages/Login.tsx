b545223961c859adb7b9f48ce8e4141103f21b46
Vixtinity <08062006ismael@gmail.com>
2025-04-25 09:47:41 +0200

    commit login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,51 @@
-export const Login = () => {
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../login.css';
+
+const Login = () => {
   return (
-    <main className="container mt-5 contenido">
-    </main>);
+    <div className="d-flex min-vh-100">
+      <div className="sidenav d-flex align-items-center justify-content-center">
+        <div className="login-main-text text-center">
+          <h2>Librería<br />Más cuento que Calleja</h2>
+          <p>Login</p>
+        </div>
+      </div>
+      <div className="main d-flex align-items-center justify-content-center">
+        <div className="col-md-6 col-sm-12">
+          <div className="login-form">
+            <form>
+              <div className="form-group">
+                <label htmlFor="nombre">Usuario</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  name="nombre"
+                  className="form-control"
+                  placeholder="Nombre de Usuario"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Contraseña</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="form-control"
+                  placeholder="Contraseña"
+                  required
+                />
+              </div>
+              <button type="submit" className="btn btn-black btn-block">
+                Acceder
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default Login;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,15 +3,18 @@ import { Index } from '../pages/Index';
 import { About } from '../pages/About';
 import { Products } from '../pages/Products';
 import { Store } from '../pages/store';
-import { Login } from '../pages/Login';
+import Login from "../pages/Login";
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      {/* Rutas generales con Header y Footer */}
       <Route path="/" element={<Index />} />
       <Route path="/about" element={<About />} />
       <Route path="/products" element={<Products />} />
       <Route path="/store" element={<Store />} />
+
+      {/* Ruta del Login sin Header ni Footer */}
       <Route path="/login" element={<Login />} />
     </Routes>
   );
